775ee7af26fc37a9bbbb585c9c8f99af428ce036
Sasha Onyshchenko <80901541+sashka-creator@users.noreply.github.com>
2023-02-13 02:24:42 +0200

    Update main.cpp

--- Game/Game/main.cpp
+++ Game/Game/main.cpp
@@ -16,11 +16,12 @@ int random(int low, int high)
 int main()
 {
     int randomEnemy = random(1, 4);
-    int randomEnemyChoise = random(1, 3);
+    int randomEnemyChoise =  random(1, 2);
     int avada = random(1, 2);
     int hp, damage;
-    string title, nameEnemy, textEnemy;
+    string title, nameEnemy, textEnemy, map, titleChoose, titleChooseEnemy;
     int hpEnemy = 10, damageEnemy;
+
 
     cout << "\t\t\t#>---------<Hogwarts Student Wars>---------<#" << endl;
     cout << "\t\t\t|                                           |" << endl;
@@ -33,7 +34,7 @@ int main()
     int numberStart;
     cin >> numberStart;
 
-   /* Character*/
+    /* Character*/
 
     if (numberStart == 1) {
 
@@ -93,94 +94,114 @@ int main()
 
     }
 
+    std::system("CLS");
     cout << "\t\t\tWelcome to " << title << ". Your HP " << hp << ". Your damage " << damage << endl;
     cout << "\t\t\t" << endl;
     cout << "\t\t\tYour enemy " << nameEnemy << ". HP " << hpEnemy << ". Damage " << damageEnemy << endl;
-    cout << "\t\t\t#>-----------------------------------------<#" << endl;
+    cout << "\t\t\t" << endl;
+    cout << "\t\t\t#>---------------<Choose map>--------------<#" << endl;
     cout << "\t\t\t|                                           |" << endl;
-    cout << "\t\t\t|              1 - Start to play            |" << endl;
-    cout << "\t\t\t|              2 - Exit                     |" << endl;
+    cout << "\t\t\t|              1 - Mirage                   |" << endl;
+    cout << "\t\t\t|              2 - Hogwarts                 |" << endl;
+    cout << "\t\t\t|              3 - Elf Bank                 |" << endl;
     cout << "\t\t\t|                                           |" << endl;
     cout << "\t\t\t#>-----------------------------------------<#" << endl;
-    int numberMiddle;
-    cin >> numberMiddle;
+    int numberMap;
+    cin >> numberMap;
 
+    if (numberMap == 1) {
 
-    if (numberMiddle == 1) {
+        map = "Mirage";
+    }
+    else if (numberMap == 2) {
 
-        while (hp >= 0 && hpEnemy >= 0) {
+        map = "Hogwarts";
+    }
+    else if (numberMap == 3) {
 
-            cout << "\t\t\tBe careful, because enemy have next move" << endl;
-            cout << "\t\t\t#>-----------------------------------------<#" << endl;
-            cout << "\t\t\t|                                           |" << endl;
-            cout << "\t\t\t|          1 - Expecto Patronum             |" << endl;
-            cout << "\t\t\t|          - 30 hp + default damage         |" << endl;
-            cout << "\t\t\t|                                           |" << endl;
-            cout << "\t\t\t|          2 - Imperio                      |" << endl;
-            cout << "\t\t\t|          + 30 hp to your character        |" << endl;
-            cout << "\t\t\t|                                           |" << endl;
-            cout << "\t\t\t|          3 - Avada Kedavra                |" << endl;
-            cout << "\t\t\t|          - 100% hp enemy or yours         |" << endl;
-            cout << "\t\t\t|                                           |" << endl;
-            cout << "\t\t\t#>-----------------------------------------<#" << endl;
-            cout << "\t\t\t                                             " << endl;
-            cout << "\t\t\t         Your hp " << hp  << endl;
-            cout << "\t\t\t         Enemy hp " << hpEnemy << endl;
-            cout << "\t\t\t                                             " << endl;
-            cout << "\t\t\t#>-----------------------------------------<#" << endl;
-            int numberEnd;
-            cin >> numberEnd;
+        map = "Elf Bank";
+    }
 
-            if (numberEnd == 1) {
+    while (hp >= 0 && hpEnemy >= 0) {
 
-                hpEnemy -= 30;
-                textEnemy = "Enemy: 'Ha ha ha! It's to easy. Take this!'";
-                cout << textEnemy << endl;
+        std::system("CLS");
+        cout << "\t\t\t" << map << "! Good choise!" << endl;
+        cout << "\t\t\t" << titleChoose << titleChooseEnemy << endl;
+        cout << "\t\t\tChoose your move!" << endl;
+        cout << "\t\t\t" << endl;
+        cout << "\t\t\t#>-----------------------------------------<#" << endl;
+        cout << "\t\t\t|                                           |" << endl;
+        cout << "\t\t\t|          1 - Expecto Patronum             |" << endl;
+        cout << "\t\t\t|          - 30 hp + default damage         |" << endl;
+        cout << "\t\t\t|                                           |" << endl;
+        cout << "\t\t\t|          2 - Imperio                      |" << endl;
+        cout << "\t\t\t|          + 30 hp to your character        |" << endl;
+        cout << "\t\t\t|                                           |" << endl;
+        cout << "\t\t\t|          3 - Avada Kedavra                |" << endl;
+        cout << "\t\t\t|          - 100% hp enemy or yours         |" << endl;
+        cout << "\t\t\t|                                           |" << endl;
+        cout << "\t\t\t#>-----------------------------------------<#" << endl;
+        cout << "\t\t\t                                             " << endl;
+        cout << "\t\t\t           Your hp " << hp << endl;
+        cout << "\t\t\t           Enemy hp " << hpEnemy << endl;
+        cout << "\t\t\t                                             " << endl;
+        cout << "\t\t\t#>-----------------------------------------<#" << endl;
+        int numberEnd;
+        cin >> numberEnd;
 
-            }
-            else if (numberEnd == 2) {
+        if (numberEnd == 1 && randomEnemyChoise == 1) {
 
-                hp += 30;
-                textEnemy = "Huh!? You so strange!";
-                cout << textEnemy << endl;
-
-            }
-            else if (numberEnd == 3) {
-
-                if (avada == 1) {
-                    hpEnemy -= 180;
-                    cout << "Oh you son of a bitch, you won" << endl;
-                }
-                else {
-                    hp -= 180;
-                    cout << "It was so ezz, I won" << endl;
-                }
-
-            }
-
-            if (randomEnemyChoise == 1 & hpEnemy > 0) {
-                hpEnemy -= 30;
-                cout << "Do you like it?" << endl;
-            }
-            else if (randomEnemyChoise == 2) {
-
-                hpEnemy += 30;
-
-            }
-        }
-
-        if (hp <= 0) {
-
-            cout << "Congratulations you lose!" << endl;
+            hpEnemy -= 30;
+            hp -= 30;
+            titleChoose = "You picked Expecto Patronum.";
+            titleChooseEnemy = "Your enemy picked Expecto Patronum.";
 
         }
-        else if (hpEnemy <= 0) {
-            cout << "Congratulations you won!" << endl;
-        }
+        else if (numberEnd == 1 && randomEnemyChoise == 2) {
 
+            hpEnemy += 30;
+            hp -= 30;
+            titleChoose = "You picked Expecto Patronum.";
+            titleChooseEnemy = "Your enemy picked Imperio.";
+
+        }
+        else if (numberEnd == 2 && randomEnemyChoise == 1) {
+
+            hpEnemy -= 30;
+            hp += 30;
+            titleChoose = "You picked Imperio.";
+            titleChooseEnemy = "Your enemy Expecto Patronum.";
+
+        }
+        else if (numberEnd == 2 && randomEnemyChoise == 2) {
+
+            hpEnemy += 30;
+            hp += 30;
+            titleChoose = "You picked Imperio.";
+            titleChooseEnemy = "Your enemy picked Imperio.";
+
+        }
+        else if (numberEnd == 3) {
+
+            if (avada == 1) {
+                hpEnemy -= 1000;
+                cout << "Oh you son of a bitch, you won" << endl;
+            }
+            else {
+                hp -= 1000;
+                cout << "It was so ezz, I won" << endl;
+            }
+
+        }
 
     }
-    else if (numberMiddle == 2) {
+
+
+    if (hp <= 0) {
+
+        std::system("CLS");
+
+        cout << "Nice try! Maybe later you will win" << endl;
 
         cout << "\t\t\t#>-----------------------------------------<#" << endl;
         cout << "\t\t\t|                                           |" << endl;
@@ -189,4 +210,17 @@ int main()
         cout << "\t\t\t#>-----------------------------------------<#" << endl;
 
     }
+    else if (hpEnemy <= 0) {
+
+        std::system("CLS");
+
+        cout << "Good job! You won!" << endl;
+        cout << "\t\t\t#>-----------------------------------------<#" << endl;
+        cout << "\t\t\t|                                           |" << endl;
+        cout << "\t\t\t|                  The End                  |" << endl;
+        cout << "\t\t\t|                                           |" << endl;
+        cout << "\t\t\t#>-----------------------------------------<#" << endl;
+
+    }
+
 }
